702ae33e545d24e23078c0d6e9dd53b336b34bfc
Chris Mahoney <chrism@unity3d.com>
2015-10-20 11:38:41 -0500

    * Updated PostBuildProcessor.cs to update a build property in the generated Xcode project (for Unity Cloud Build and local editor build).

--- Assets/Editor/PostBuildProcessor.cs
+++ Assets/Editor/PostBuildProcessor.cs
@@ -10,6 +10,11 @@ using System.Linq;
 
 public class PostBuildProcessor : MonoBehaviour
 {
+	/**
+	 * Runs when Post-Export method has been set to 
+	 * 'PostBuildProcessor.OnPostprocessBuildiOS' in your Unity Cloud Build
+	 * target settings.
+	 */
 	#if UNITY_CLOUD_BUILD
 	// This method is added in the Advanced Features Settings on UCB
 	// PostBuildProcessor.OnPostprocessBuildiOS
@@ -20,7 +25,10 @@ public class PostBuildProcessor : MonoBehaviour
 	}
 	#endif
 	
-	
+	/**
+	 * Runs after successful build of an iOS-targetted Unity project
+	 * via the editor Build dialog.
+	 */
 	[PostProcessBuild]
 	public static void OnPostprocessBuild (BuildTarget buildTarget, string path)
 	{
@@ -29,58 +37,42 @@ public class PostBuildProcessor : MonoBehaviour
 		ProcessPostBuild (buildTarget, path);
 		#endif
 	}
-	
-	internal static void CopyAndReplaceDirectory (string srcPath, string dstPath)
-	{		
-		if (!Directory.Exists (dstPath))
-			Directory.CreateDirectory (dstPath);
-		foreach (var file in Directory.GetFiles(srcPath))
-			File.Copy (file, Path.Combine (dstPath, Path.GetFileName (file)));
-		
-		foreach (var dir in Directory.GetDirectories(srcPath))
-			CopyAndReplaceDirectory (dir, Path.Combine (dstPath, Path.GetFileName (dir)));
-	}
-	
+
+	/**
+	 * This ProcessPostBuild method will run via Unity Cloud Build, as well as 
+	 * locally when build target is iOS. Using the Xcode Manipulation API, it is
+	 * possible to modify build settings values and also perform other actions
+	 * such as adding custom frameworks. Link below is the reference documentation
+	 * for the Xcode Manipulation API:
+	 * 
+	 * http://docs.unity3d.com/ScriptReference/iOS.Xcode.PBXProject.html
+	 */
 	private static void ProcessPostBuild (BuildTarget buildTarget, string path)
 	{
+		// This code will set modules to enabled in Xcode build settings
 		#if UNITY_IOS
-		// We only need this IsSceneActive because we do not want to include the library data in other examples then the LibraryDemo
-		if (buildTarget == BuildTarget.iOS && IsSceneActive("Assets/Scene/GoogleAdsUnity.unity")) {
 
-			/*
-			Debug.Log ("[UNITY_IOS] ProcessPostBuild - Xcode Manipulation API");
+		Debug.Log ("[UNITY_IOS] ProcessPostBuild - Xcode Manipulation API");
+		
+		// Go get pbxproj file
+		string projPath = path + "/Unity-iPhone.xcodeproj/project.pbxproj";
 			
-			string projPath = path + "/Unity-iPhone.xcodeproj/project.pbxproj";
-				
-			PBXProject proj = new PBXProject ();
-			proj.ReadFromString (File.ReadAllText (projPath));
-			
-			string target = proj.TargetGuidByName ("Unity-iPhone");
+		// PBXProject class represents a project build settings file,
+		// here is how to read that in.
+		PBXProject proj = new PBXProject ();
+		proj.ReadFromString (File.ReadAllText (projPath));
 
-			Debug.Log("Enabling modules: CLANG_ENABLE_MODULES = YES");
-			proj.AddBuildProperty(target, "CLANG_ENABLE_MODULES", "YES");
+		// This is the Xcode target in the generated project
+		string target = proj.TargetGuidByName ("Unity-iPhone");
 
-			File.WriteAllText (projPath, proj.WriteToString ());
-			*/
+		// Here we go: Set 'Enable Modules' to YES to prevent errors using @import syntax
+		Debug.Log("Enabling modules: CLANG_ENABLE_MODULES = YES");
+		proj.AddBuildProperty(target, "CLANG_ENABLE_MODULES", "YES");
 
-		}
+		// Write PBXProject object back to the file
+		File.WriteAllText (projPath, proj.WriteToString ());
+
 		#endif
-	}
-	
-	static bool IsSceneActive (string sceneName)
-	{
-		string[] levels = FillLevels ();
-		for (int i = 0; i < levels.Length; ++i) {
-			if (levels [i] == sceneName) {
-				return true;
-			}
-		}
-		return false;
-	}
-	
-	private static string[] FillLevels ()
-	{
-		return (from scene in EditorBuildSettings.scenes where scene.enabled select scene.path).ToArray ();
 	}
 }
 
